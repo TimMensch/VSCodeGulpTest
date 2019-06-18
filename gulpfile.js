9d6a3d9d458d5ccfc36022d180d998db884a6e88
Tim Mensch <tim-sg-chimera@bitgems.com>
2019-06-18 09:32:30 -0600

    initial commit: First attempt.

---
+++ gulpfile.js
@@ -0,0 +1,509 @@
+const gulp = require("gulp");
+
+// for (let i = 1; i < 100; ++i) {
+//     console.log(`
+// gulp.task("task${i}", ["task${i - 1}"], function(cb) {
+//     console.log("task${i}");
+//     cb();
+// });`);
+// }
+
+gulp.task("task0", function(cb) {
+    console.log("task0");
+    cb();
+});
+
+gulp.task("task1", ["task0"], function(cb) {
+    console.log("task1");
+    cb();
+});
+
+gulp.task("task2", ["task1"], function(cb) {
+    console.log("task2");
+    cb();
+});
+
+gulp.task("task3", ["task2"], function(cb) {
+    console.log("task3");
+    cb();
+});
+
+gulp.task("task4", ["task3"], function(cb) {
+    console.log("task4");
+    cb();
+});
+
+gulp.task("task5", ["task4"], function(cb) {
+    console.log("task5");
+    cb();
+});
+
+gulp.task("task6", ["task5"], function(cb) {
+    console.log("task6");
+    cb();
+});
+
+gulp.task("task7", ["task6"], function(cb) {
+    console.log("task7");
+    cb();
+});
+
+gulp.task("task8", ["task7"], function(cb) {
+    console.log("task8");
+    cb();
+});
+
+gulp.task("task9", ["task8"], function(cb) {
+    console.log("task9");
+    cb();
+});
+
+gulp.task("task10", ["task9"], function(cb) {
+    console.log("task10");
+    cb();
+});
+
+gulp.task("task11", ["task10"], function(cb) {
+    console.log("task11");
+    cb();
+});
+
+gulp.task("task12", ["task11"], function(cb) {
+    console.log("task12");
+    cb();
+});
+
+gulp.task("task13", ["task12"], function(cb) {
+    console.log("task13");
+    cb();
+});
+
+gulp.task("task14", ["task13"], function(cb) {
+    console.log("task14");
+    cb();
+});
+
+gulp.task("task15", ["task14"], function(cb) {
+    console.log("task15");
+    cb();
+});
+
+gulp.task("task16", ["task15"], function(cb) {
+    console.log("task16");
+    cb();
+});
+
+gulp.task("task17", ["task16"], function(cb) {
+    console.log("task17");
+    cb();
+});
+
+gulp.task("task18", ["task17"], function(cb) {
+    console.log("task18");
+    cb();
+});
+
+gulp.task("task19", ["task18"], function(cb) {
+    console.log("task19");
+    cb();
+});
+
+gulp.task("task20", ["task19"], function(cb) {
+    console.log("task20");
+    cb();
+});
+
+gulp.task("task21", ["task20"], function(cb) {
+    console.log("task21");
+    cb();
+});
+
+gulp.task("task22", ["task21"], function(cb) {
+    console.log("task22");
+    cb();
+});
+
+gulp.task("task23", ["task22"], function(cb) {
+    console.log("task23");
+    cb();
+});
+
+gulp.task("task24", ["task23"], function(cb) {
+    console.log("task24");
+    cb();
+});
+
+gulp.task("task25", ["task24"], function(cb) {
+    console.log("task25");
+    cb();
+});
+
+gulp.task("task26", ["task25"], function(cb) {
+    console.log("task26");
+    cb();
+});
+
+gulp.task("task27", ["task26"], function(cb) {
+    console.log("task27");
+    cb();
+});
+
+gulp.task("task28", ["task27"], function(cb) {
+    console.log("task28");
+    cb();
+});
+
+gulp.task("task29", ["task28"], function(cb) {
+    console.log("task29");
+    cb();
+});
+
+gulp.task("task30", ["task29"], function(cb) {
+    console.log("task30");
+    cb();
+});
+
+gulp.task("task31", ["task30"], function(cb) {
+    console.log("task31");
+    cb();
+});
+
+gulp.task("task32", ["task31"], function(cb) {
+    console.log("task32");
+    cb();
+});
+
+gulp.task("task33", ["task32"], function(cb) {
+    console.log("task33");
+    cb();
+});
+
+gulp.task("task34", ["task33"], function(cb) {
+    console.log("task34");
+    cb();
+});
+
+gulp.task("task35", ["task34"], function(cb) {
+    console.log("task35");
+    cb();
+});
+
+gulp.task("task36", ["task35"], function(cb) {
+    console.log("task36");
+    cb();
+});
+
+gulp.task("task37", ["task36"], function(cb) {
+    console.log("task37");
+    cb();
+});
+
+gulp.task("task38", ["task37"], function(cb) {
+    console.log("task38");
+    cb();
+});
+
+gulp.task("task39", ["task38"], function(cb) {
+    console.log("task39");
+    cb();
+});
+
+gulp.task("task40", ["task39"], function(cb) {
+    console.log("task40");
+    cb();
+});
+
+gulp.task("task41", ["task40"], function(cb) {
+    console.log("task41");
+    cb();
+});
+
+gulp.task("task42", ["task41"], function(cb) {
+    console.log("task42");
+    cb();
+});
+
+gulp.task("task43", ["task42"], function(cb) {
+    console.log("task43");
+    cb();
+});
+
+gulp.task("task44", ["task43"], function(cb) {
+    console.log("task44");
+    cb();
+});
+
+gulp.task("task45", ["task44"], function(cb) {
+    console.log("task45");
+    cb();
+});
+
+gulp.task("task46", ["task45"], function(cb) {
+    console.log("task46");
+    cb();
+});
+
+gulp.task("task47", ["task46"], function(cb) {
+    console.log("task47");
+    cb();
+});
+
+gulp.task("task48", ["task47"], function(cb) {
+    console.log("task48");
+    cb();
+});
+
+gulp.task("task49", ["task48"], function(cb) {
+    console.log("task49");
+    cb();
+});
+
+gulp.task("task50", ["task49"], function(cb) {
+    console.log("task50");
+    cb();
+});
+
+gulp.task("task51", ["task50"], function(cb) {
+    console.log("task51");
+    cb();
+});
+
+gulp.task("task52", ["task51"], function(cb) {
+    console.log("task52");
+    cb();
+});
+
+gulp.task("task53", ["task52"], function(cb) {
+    console.log("task53");
+    cb();
+});
+
+gulp.task("task54", ["task53"], function(cb) {
+    console.log("task54");
+    cb();
+});
+
+gulp.task("task55", ["task54"], function(cb) {
+    console.log("task55");
+    cb();
+});
+
+gulp.task("task56", ["task55"], function(cb) {
+    console.log("task56");
+    cb();
+});
+
+gulp.task("task57", ["task56"], function(cb) {
+    console.log("task57");
+    cb();
+});
+
+gulp.task("task58", ["task57"], function(cb) {
+    console.log("task58");
+    cb();
+});
+
+gulp.task("task59", ["task58"], function(cb) {
+    console.log("task59");
+    cb();
+});
+
+gulp.task("task60", ["task59"], function(cb) {
+    console.log("task60");
+    cb();
+});
+
+gulp.task("task61", ["task60"], function(cb) {
+    console.log("task61");
+    cb();
+});
+
+gulp.task("task62", ["task61"], function(cb) {
+    console.log("task62");
+    cb();
+});
+
+gulp.task("task63", ["task62"], function(cb) {
+    console.log("task63");
+    cb();
+});
+
+gulp.task("task64", ["task63"], function(cb) {
+    console.log("task64");
+    cb();
+});
+
+gulp.task("task65", ["task64"], function(cb) {
+    console.log("task65");
+    cb();
+});
+
+gulp.task("task66", ["task65"], function(cb) {
+    console.log("task66");
+    cb();
+});
+
+gulp.task("task67", ["task66"], function(cb) {
+    console.log("task67");
+    cb();
+});
+
+gulp.task("task68", ["task67"], function(cb) {
+    console.log("task68");
+    cb();
+});
+
+gulp.task("task69", ["task68"], function(cb) {
+    console.log("task69");
+    cb();
+});
+
+gulp.task("task70", ["task69"], function(cb) {
+    console.log("task70");
+    cb();
+});
+
+gulp.task("task71", ["task70"], function(cb) {
+    console.log("task71");
+    cb();
+});
+
+gulp.task("task72", ["task71"], function(cb) {
+    console.log("task72");
+    cb();
+});
+
+gulp.task("task73", ["task72"], function(cb) {
+    console.log("task73");
+    cb();
+});
+
+gulp.task("task74", ["task73"], function(cb) {
+    console.log("task74");
+    cb();
+});
+
+gulp.task("task75", ["task74"], function(cb) {
+    console.log("task75");
+    cb();
+});
+
+gulp.task("task76", ["task75"], function(cb) {
+    console.log("task76");
+    cb();
+});
+
+gulp.task("task77", ["task76"], function(cb) {
+    console.log("task77");
+    cb();
+});
+
+gulp.task("task78", ["task77"], function(cb) {
+    console.log("task78");
+    cb();
+});
+
+gulp.task("task79", ["task78"], function(cb) {
+    console.log("task79");
+    cb();
+});
+
+gulp.task("task80", ["task79"], function(cb) {
+    console.log("task80");
+    cb();
+});
+
+gulp.task("task81", ["task80"], function(cb) {
+    console.log("task81");
+    cb();
+});
+
+gulp.task("task82", ["task81"], function(cb) {
+    console.log("task82");
+    cb();
+});
+
+gulp.task("task83", ["task82"], function(cb) {
+    console.log("task83");
+    cb();
+});
+
+gulp.task("task84", ["task83"], function(cb) {
+    console.log("task84");
+    cb();
+});
+
+gulp.task("task85", ["task84"], function(cb) {
+    console.log("task85");
+    cb();
+});
+
+gulp.task("task86", ["task85"], function(cb) {
+    console.log("task86");
+    cb();
+});
+
+gulp.task("task87", ["task86"], function(cb) {
+    console.log("task87");
+    cb();
+});
+
+gulp.task("task88", ["task87"], function(cb) {
+    console.log("task88");
+    cb();
+});
+
+gulp.task("task89", ["task88"], function(cb) {
+    console.log("task89");
+    cb();
+});
+
+gulp.task("task90", ["task89"], function(cb) {
+    console.log("task90");
+    cb();
+});
+
+gulp.task("task91", ["task90"], function(cb) {
+    console.log("task91");
+    cb();
+});
+
+gulp.task("task92", ["task91"], function(cb) {
+    console.log("task92");
+    cb();
+});
+
+gulp.task("task93", ["task92"], function(cb) {
+    console.log("task93");
+    cb();
+});
+
+gulp.task("task94", ["task93"], function(cb) {
+    console.log("task94");
+    cb();
+});
+
+gulp.task("task95", ["task94"], function(cb) {
+    console.log("task95");
+    cb();
+});
+
+gulp.task("task96", ["task95"], function(cb) {
+    console.log("task96");
+    cb();
+});
+
+gulp.task("task97", ["task96"], function(cb) {
+    console.log("task97");
+    cb();
+});
+
+gulp.task("task98", ["task97"], function(cb) {
+    console.log("task98");
+    cb();
+});
+
+gulp.task("task99", ["task98"], function(cb) {
+    console.log("task99");
+    cb();
+});
